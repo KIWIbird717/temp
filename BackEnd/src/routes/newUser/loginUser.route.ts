e5b17944d15d218bde83692e4a15bf9546273803
ArtemkaDev <87724011+ArtemkaDev@users.noreply.github.com>
2023-04-21 22:41:43 +0300

    fix

--- BackEnd/src/routes/newUser/loginUser.route.ts
+++ BackEnd/src/routes/newUser/loginUser.route.ts
@@ -1,21 +1,34 @@
-import express, { Router, Request, Response } from "express"
-import CreateNewUser from '../../servises/RegisterUserDB/addRegisterUser.servise'
-import { IRegisterUserSchema, RegisterUserSchema } from '../../servises/RegisterUserDB/registerUserSchema.servise';
+import express, { Router, Request, Response } from "express";
+import CreateNewUser from "../../servises/RegisterUserDB/addRegisterUser.servise";
+import {
+  IRegisterUserSchema,
+  RegisterUserSchema,
+} from "../../servises/RegisterUserDB/registerUserSchema.servise";
 import { customCompareDecription } from "../../utils/hooks/customCompareDecryption.util";
 
-const router: Router = express.Router()
+const router: Router = express.Router();
 
-router.post('/login', async (req: Request, res: Response) => {
-  const { mail, password } = req.body
-    
+router.post("/login", async (req: Request, res: Response) => {
+  const { mail, password } = req.body;
+
+  if (
+    !password ||
+    typeof password !== "string" ||
+    !mail ||
+    typeof mail !== "string"
+  ) {
+    return res.status(400).json({ message: "Uncurrect user" });
+  }
+
   // Check if user already exists
-  const existingUser: IRegisterUserSchema | null = await RegisterUserSchema.findOne({$or: [{ mail }]})
+  const existingUser: IRegisterUserSchema | null =
+    await RegisterUserSchema.findOne({ $or: [{ mail }] });
 
   if (await customCompareDecription(password, existingUser.password)) {
-    return res.status(201).json({ message: 'User logined successfully' })
+    return res.status(201).json({ message: "User logined successfully" });
   } else {
-    return res.status(400).json({ message: 'Uncurrect password' })
+    return res.status(400).json({ message: "Uncurrect password" });
   }
-})
+});
 
 export default router;
